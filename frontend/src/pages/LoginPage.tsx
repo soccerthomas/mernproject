306330c97322eda6b00c6566d03b834b6f386f94
justinmoreira <justinmoreira23@gmail.com>
2025-06-24 16:46:50 -0400

    Logo reroute to dashboard

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -18,7 +18,7 @@ function Login() {
       <header className="bg-white">
         <nav className="mx-auto flex max-w-7xl items-center justify-between p-4 lg:px-8 border-2" aria-label="Global">
           <div className="flex lg:flex-1">
-            <a href="#" className="-m-1.5 p-1.5">
+            <a href="./" className="-m-1.5 p-1.5">
               <span className="sr-only">Tier Builder</span>
               <img className="h-8 w-auto" src={TierListLogo} alt="" />
             </a>
