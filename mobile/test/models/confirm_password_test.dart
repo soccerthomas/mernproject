dbbdadf8f572b055797da8608731d03eb48f7814
Matt-Salamone <175448127+Matt-Salamone@users.noreply.github.com>
2025-07-19 14:49:10 -0400

    Add unit testing for password and confirm password

---
+++ mobile/test/models/confirm_password_test.dart
@@ -0,0 +1,16 @@
+import 'package:test/test.dart';
+import 'package:mobile/login_register/models/confirm_password.dart';
+
+void main() {
+  group('ConfirmPassword Model', () {
+    test('should return empty error when confirmPassword is empty', () {
+      const confirmPassword = ConfirmPassword.dirty(value: '', password: '');
+      expect(confirmPassword.error, ConfirmPasswordValidationError.empty);
+    });
+
+    test('should return mismatch error when confirmPassword does not match password', () {
+      const confirmPassword = ConfirmPassword.dirty(value: '12345678', password: '11345678');
+      expect(confirmPassword.error, ConfirmPasswordValidationError.mismatch);
+    });
+  });
+}
