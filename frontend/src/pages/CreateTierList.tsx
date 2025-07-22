a6e730c005847253035529c4dcd3dc71011fe01e
justinmoreira <justinmoreira23@gmail.com>
2025-07-22 14:10:02 -0400

    Show modal only on create

--- frontend/src/pages/CreateTierList.tsx
+++ frontend/src/pages/CreateTierList.tsx
@@ -711,7 +711,7 @@ function CreateTierList() {
           Save Tier List
         </button>
       </div>
-      {showInfoModal && (
+      {showInfoModal && !isEditing && (
         <div className="fixed inset-0 bg-black bg-opacity-60 flex items-center justify-center z-50 p-4">
           <div className="bg-gray-700 max-h-[95vh] w-full max-w-6xl rounded-2xl relative overflow-hidden">
             <div className="bg-blue-500 p-6 text-center">
